7f3b5ad9f27dbc56f497eab727bd8fbf76e8194c
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-10-28 15:14:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,9 @@
+ def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-
-def display_board
-puts "A Tic Tac Toe Board"
 end
 
-display_board
-A Tic Tac Toe Board
-nil
+
